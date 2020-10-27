97db1632300dd010d539f7cfb46639176faeddd2
tytanus97 <tytanus97@gmail.com>
2020-10-27 13:23:51 +0100

    change project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'authenticate',
-    loadChildren: () => import('./authenticate/authenticate.module').then( m => m.AuthenticatePageModule)
-  },
+  
 ];
 
 @NgModule({
