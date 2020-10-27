c1ee63a5315a6b85dbe2625bb9551d15ba37566d
tytanus97 <tytanus97@gmail.com>
2020-10-27 14:28:07 +0100

    add mocked authentication service

---
+++ src/app/services/authentication/auth.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private _isLogged: boolean = false;
+
+  constructor(private httpClient: HttpClient) {
+
+    setTimeout(() => {
+
+      this.logOut();
+    },1000*60*60*8);
+
+   }
+
+  isLogged() {
+    return this._isLogged;
+  }
+
+  logIn() {
+    if(!this._isLogged) {
+      this._isLogged = true;
+    }
+  }
+  logOut() {
+    if(!this._isLogged) {
+      this._isLogged = false;
+    }
+  }
+}
