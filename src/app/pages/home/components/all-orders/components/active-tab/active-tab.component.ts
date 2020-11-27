7b7486c1f268ccd7c495e74381a8f6eba7450254
tytanus97 <tytanus97@gmail.com>
2020-11-27 18:13:07 +0100

    refactor drop down animation

--- src/app/pages/home/components/all-orders/components/active-tab/active-tab.component.ts
+++ src/app/pages/home/components/all-orders/components/active-tab/active-tab.component.ts
@@ -1,6 +1,7 @@
-import { AfterViewInit, ApplicationRef, ChangeDetectorRef, Component, DoCheck, ElementRef, KeyValueDiffers, OnInit, QueryList, Renderer2, ViewChildren,  } from '@angular/core';
-import { ActivatedRoute  } from '@angular/router';
+import { AfterViewInit, ApplicationRef, ChangeDetectorRef, Component, DoCheck, ElementRef, KeyValueDiffers, OnInit, QueryList, Renderer2, ViewChildren, } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AlertController, GestureController, IonCard, ModalController } from '@ionic/angular';
+import { drop } from 'lodash';
 import { Order } from 'src/app/models/order';
 import { OrderedDish } from 'src/app/models/orderedDish';
 import { OrdersService } from 'src/app/services/orders/orders.service';
@@ -11,14 +12,15 @@ import { ChooseDishComponent } from '../../../add-order/components/choose-dish/c
   templateUrl: './active-tab.component.html',
   styleUrls: ['./active-tab.component.scss'],
 })
-export class ActiveTabComponent implements OnInit,AfterViewInit {
+export class ActiveTabComponent implements OnInit, AfterViewInit {
 
   public activeOrders: Array<Order>;
   private _today: Date;
   public trigger: boolean = false;
+  private _longPressActive = false;
 
 
-  @ViewChildren(IonCard,{read:ElementRef})private ordersCards: QueryList<ElementRef>;
+  @ViewChildren('cardContainer', { read: ElementRef }) private ordersCards: QueryList<ElementRef>;
 
   constructor(private _ordersService: OrdersService,
     private _route: ActivatedRoute,
@@ -26,7 +28,7 @@ export class ActiveTabComponent implements OnInit,AfterViewInit {
     private _modalCtrl: ModalController,
     private _gestureCtrl: GestureController,
     private _renderer: Renderer2) {
- 
+
     this._today = new Date();
 
     this._route.data.subscribe(result => {
@@ -35,17 +37,17 @@ export class ActiveTabComponent implements OnInit,AfterViewInit {
   }
 
   ngOnInit() {
-    
+
   }
 
   ngAfterViewInit() {
-    
+
     const cardArr = this.ordersCards.toArray();
-    
+
     this.bindLongPress(cardArr);
 
   }
-  
+
   public async changeStatus(orderedDish: OrderedDish, order: Order) {
     const alert = await this._alertController.create({
       message: 'Oznaczyć jako dostarczone?',
@@ -57,8 +59,8 @@ export class ActiveTabComponent implements OnInit,AfterViewInit {
         {
           text: 'OK',
           handler: () => {
-            
-            this._checkIfAllDelivered(order,orderedDish);
+
+            this._checkIfAllDelivered(order, orderedDish);
           }
         }
       ]
@@ -66,7 +68,7 @@ export class ActiveTabComponent implements OnInit,AfterViewInit {
     await alert.present();
   }
 
-  private _checkIfAllDelivered(order: Order,orderedDish:OrderedDish) {
+  private _checkIfAllDelivered(order: Order, orderedDish: OrderedDish) {
     orderedDish.orderDishStatus = 'delivered';
     const result = order.orderedDishes.filter(o => o.orderDishStatus !== 'delivered').length;
     if (!result) {
@@ -100,26 +102,54 @@ export class ActiveTabComponent implements OnInit,AfterViewInit {
 
   private bindLongPress(cardArr: Array<ElementRef>) {
     console.log(cardArr);
-    
-    cardArr.forEach((item:ElementRef) => {
+
+    cardArr.forEach((item: ElementRef) => {
       console.log(item);
       const gesture = this._gestureCtrl.create({
         el: item.nativeElement,
-        gestureName:'long-press',
-        onStart:() =>{
-          setTimeout(() => {
-            const lastChild = item.nativeElement.lastChild;
-            this._renderer.addClass(lastChild,"drop-down");
-            console.log('long press started');
-          },500);
-        }  
-        //onEnd:() => console.log('long press stop'),
-        
-        
+        gestureName: 'long-press',
+        onStart: () => {
+          console.log('long press start');
+          this._longPressActive = true;
+          this.countPressDuration(0,item);
+        },
+        onEnd: () => {
+          this._longPressActive = false;
+          console.log('long press stop');
+        }
       })
-
       gesture.enable(true);
     })
+  }
+
+  private countPressDuration(pressDuration: number,element: ElementRef) {
+    
+    setTimeout(() => {
+      if(this._longPressActive) {
+        console.log(pressDuration);
+        pressDuration++;
+        if(pressDuration > 3) {
+          console.log('XD');
+            this._longPressActive = false;
+            const lastChild = element.nativeElement.lastChild;  
+            const dropDownAnimation : Animation =  lastChild.animate(
+            [
+              {transform:'translateY(-100%)',height:0,opacity:0},
+              {
+                opacity:0
+              },
+              {transform:'translateY(0)',height:'initial',opacity:1}
+            ],
+            {
+              duration:500,
+              fill:'forwards',
+              easing:'ease-out'
+            });
+        } else {
+          this.countPressDuration(pressDuration,element);
+        }
+      }
+    },300);
   }
 }
 
