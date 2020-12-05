1ea439e22cf91765265e0b546f7f44e6ffba8bd3
tytanus97 <tytanus97@gmail.com>
2020-12-05 21:27:00 +0100

    add raport and change ordered dish display

--- src/app/pages/home/components/raport/raport.component.ts
+++ src/app/pages/home/components/raport/raport.component.ts
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./raport.component.scss'],
 })
 export class RaportComponent implements OnInit {
-
+  public date: Date;
+  public monthShort = ['Sty','Lut','Mar','Kwi','Maj','Cze','Lip','Sie','Wrz','Paz','Lis','Gru'];
   constructor() { }
 
   ngOnInit() {}
 
+  public dateChanged(event) {
+    console.log(event);
+    console.log(this.date);
+  }
 }
